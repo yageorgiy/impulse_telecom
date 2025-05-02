c12ed951287fe4074a756919ba446920c73fbf43
Yankovskiy Georgiy <admin@yageorgiy.ru>
2025-05-02 19:50:25 +0300

    XML to meta.json

--- impulse_telecom/main.py
+++ impulse_telecom/main.py
@@ -1,11 +1,49 @@
 import json
+import re
 # import os
 # import traceback
 import xml.etree.ElementTree as ET
+from dataclasses import dataclass
+from typing import Any
 
 
-def build_xml_root_child_attributes(
-    current_child: ET.Element, root: ET.Element
+@dataclass
+class MetaDescriptionParameter:
+    name: str
+    type: str
+
+
+@dataclass
+class MetaDescription:
+    class_: str
+    documentation: str
+    isRoot: bool
+    max: str
+    min: str
+    parameters: list[MetaDescriptionParameter]
+
+
+class MetaDescriptionEncoder(json.JSONEncoder):
+    def default(self, obj: Any) -> Any:
+        if isinstance(obj, MetaDescription):
+            dict = obj.__dict__
+
+            if "max" in dict and dict["max"] == "-":
+                del dict["max"]
+
+            if "min" in dict and dict["min"] == "-":
+                del dict["min"]
+
+            return dict
+        if isinstance(obj, MetaDescriptionParameter):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
+
+
+def build_child_attributes(
+    current_child: ET.Element,
+    root: ET.Element,
+    json_meta_parameters: list[MetaDescriptionParameter]
 ) -> None:
     for child in current_child:
         tag: str = child.tag
@@ -13,8 +51,8 @@ def build_xml_root_child_attributes(
             continue
 
         child_attrib: dict[str, str] = child.attrib
-        attribute_name: str = child_attrib.get("name") or ""
-        attribute_type: str = child_attrib.get("type") or ""
+        attribute_name: str = child_attrib.get("name", "")
+        attribute_type: str = child_attrib.get("type", "")
 
         if attribute_name == "":
             continue
@@ -22,66 +60,123 @@ def build_xml_root_child_attributes(
         attribute = ET.SubElement(root, attribute_name)
         attribute.text = attribute_type
 
+        json_meta_parameters.append(MetaDescriptionParameter(
+            name=attribute_name,
+            type=attribute_type
+        ))
 
-def build_xml(root_tree: ET.Element) -> None:
-    new_root: ET.Element | None = None
-    new_classes_dict: dict[str, ET.Element] = dict()
-    classes_list: list[ET.Element] = []
-    aggregation_list: list[ET.Element] = []
+
+def process(
+    patched_config: dict[str, int],
+    config: dict[str, int],
+    impulse_test_input_tree: ET.ElementTree,
+) -> None:
+    xml_root_tree: ET.Element = impulse_test_input_tree.getroot()
+    xml_new_root: ET.Element | None = None
+    xml_new_classes_dict: dict[str, ET.Element] = dict()
+    xml_classes_list: list[ET.Element] = []
+    xml_aggregation_list: list[ET.Element] = []
+
+    json_meta: dict[str, MetaDescription] = dict()
 
     # Find all tags with Classes and Aggregations
     # Put items to lists again to avoid incorrect XML-order
-    for child in root_tree:
+    for child in xml_root_tree:
         # print(child.tag, child.attrib)
         tag = child.tag
         if tag == "Class":
-            classes_list.append(child)
-            # classes_dict[child.attrib.get('name') or '_'] = child
+            xml_classes_list.append(child)
+            # classes_dict[child.attrib.get('name', '_')] = child
         elif tag == "Aggregation":
-            aggregation_list.append(child)
+            xml_aggregation_list.append(child)
 
     # Process Classes
     # for key, class_child in classes_dict.items():
-    for class_child in classes_list:
+    for class_child in xml_classes_list:
         attrib: dict[str, str] = class_child.attrib
-        class_name: str = attrib.get("name") or "_"
+        class_name: str = attrib.get("name", "_")
         isRoot: bool = attrib.get("isRoot") == "true"
 
-        if class_name not in new_classes_dict:
-            e = ET.Element(class_name)
-            new_classes_dict[class_name] = e
-
-            if isRoot and new_root is None:
-                new_root = e
-
-            build_xml_root_child_attributes(class_child, e)
-        else:
+        if class_name in xml_new_classes_dict:
             print("Warning! Got duplication.", class_child)
+            continue
 
-    if new_root is None:
-        print("No root found for XML.")
+        e = ET.Element(class_name)
+        xml_new_classes_dict[class_name] = e
+
+        if isRoot and xml_new_root is None:
+            xml_new_root = e
+
+        # Building basic MetaDescription for meta.json
+        json_meta_parameters: list[MetaDescriptionParameter] = []
+        build_child_attributes(class_child, e, json_meta_parameters)
+
+        json_meta[class_name] = MetaDescription(
+            class_=class_name,
+            parameters=json_meta_parameters,
+            isRoot=isRoot,
+            documentation=attrib.get("documentation", ""),
+            # placeholder, calculated later, should be ignored if "-"
+            max="-",
+            # placeholder, calculated later, should be ignored if "-"
+            min="-"
+        )
+
+    if xml_new_root is None:
+        print("No root found.")
         return
 
     print("Built base.")
-    # ET.dump(new_root)
+    # ET.dump(xml_new_root)
 
     # Process Aggregations
-    for aggregation_child in aggregation_list:
-        source: str = aggregation_child.get("source") or ""
-        target: str = aggregation_child.get("target") or ""
+    for aggregation_child in xml_aggregation_list:
+        source: str = aggregation_child.get("source", "")
+        target: str = aggregation_child.get("target", "")
 
         if source == "" or target == "":
             continue
 
-        if source not in new_classes_dict or target not in new_classes_dict:
+        if (source not in xml_new_classes_dict or
+                target not in xml_new_classes_dict):
             continue
 
-        new_classes_dict[target].append(new_classes_dict[source])
+        xml_new_classes_dict[target].append(xml_new_classes_dict[source])
+
+        json_meta[target].parameters.append(MetaDescriptionParameter(
+            name=source,
+            type="class"
+        ))
+
+        source_multiplicity: str = (
+            aggregation_child.get("sourceMultiplicity", "1"))
+        # Trying to find "number..number" in attribute
+        # match: re.Match[str] | None = (
+        # re.search(r'([0-9]+)\.\.([0-9]+)', source_multiplicity))
+        match: list[tuple[str, str]] = (
+            re.findall(r'([0-9]+)\.\.([0-9]+)', source_multiplicity))
+
+        # should be ignored if 0
+        _min: str = "0"
+        _max: str = "0"
+
+        if match and match[0]:
+            data: tuple[str, str] = match[0]
+            _min = data[0]
+            _max = data[1]
+        # elif isinstance(source_multiplicity, int):
+        else:
+            _min = source_multiplicity
+            _max = source_multiplicity
+
+        json_meta[source].max = _max
+        json_meta[source].min = _min
 
     print("Built connections.")
 
+    # XML export
     rough_xml_contents: bytes = ET.tostring(
-        new_root,
+        xml_new_root,
         encoding="utf-8",
         # Example file elements are not shortened
         short_empty_elements=False,
@@ -93,13 +188,20 @@ def build_xml(root_tree: ET.Element) -> None:
         # out_file.write(parsed.toprettyxml(indent="\t"))
         print("Written to out/config.xml.")
 
-
-def process(
-    patched_config: dict[str, int],
-    config: dict[str, int],
-    impulse_test_input_tree: ET.ElementTree,
-) -> None:
-    build_xml(impulse_test_input_tree.getroot())
+    # JSON meta.json export
+    with open("out/meta.json", mode="w") as out_file:
+        print(json_meta)
+        out_file.write(json.dumps(
+            # From dict to list
+            list(json_meta.values()),
+            # Allow utf-8 characters
+            ensure_ascii=False,
+            # Pretty print
+            indent=4,
+            # Custom encoder for dataclasses
+            cls=MetaDescriptionEncoder
+        ))
+        print("Written to out/meta.json.")
 
 
 def main() -> None:
